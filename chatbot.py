d5689ad565780f4a44874db65a1b83c94257bb10
John Ferry Lagman <johnferrydizonlagman@gmail.com>
2025-05-24 16:13:43 +0800

    feat: gemini responses

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,13 @@
 from google import genai
 from dotenv import load_dotenv
 import streamlit as st
+import os
 
 load_dotenv()
 
+api_key = os.getenv('GEMINI_API_KEY')
 # replace this later to use the env variable!!
-client = genai.Client(api_key="os.getenv('GEMINI_API_KEY')")
+client = genai.Client(api_key=api_key)
 
 
 
@@ -26,9 +28,14 @@ for message in st.session_state.history:
 if prompt := st.chat_input("Say something", accept_file=True, file_type=["pdf"]):
     # Save user message
     st.session_state.history.append({"role": "user", "content": prompt["text"]})
-    messages.chat_message("user").write(prompt["file"])
+    messages.chat_message("user").write(prompt["text"])
+    
     
     # Generate and save assistant reply
-    reply = f"Echo: {prompt["text"]}"
+    response = client.models.generate_content(
+        model = "gemini-2.0-flash",
+        contents=[prompt["text"]]
+    )
+    reply = f"Gemini: {response.candidates[0].content.parts[0].text}"
     st.session_state.history.append({"role": "assistant", "content": reply})
     messages.chat_message("assistant").write(reply)
